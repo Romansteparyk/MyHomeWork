7266ed990a2cefbca6d266b53f5c38037f79ea3e
Romansteparyk <131956622+Romansteparyk@users.noreply.github.com>
2023-05-03 17:01:42 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+# Подходит ли топливо для доменной печи
+
+# Тип топлива
+
+name = 'coal'
+print(type(name),name)
+print()
+# Содержание углерода в процентах
+
+carbon_content = 90
+print(type(carbon_content),carbon_content,'%')
+print()
+
+# Зольность в процентах
+
+ash_content = 9
+print(type(ash_content),ash_content,'%')
+print()
+
+# Теплоотдача килокалорий на килограмм
+
+heart_transfer = 8650.50
+print(type(heart_transfer),heart_transfer,'кКал/кг')
+print()
+
+# Коксующийся (Подходит или нет)
+
+caking_coal = True
+print(type(caking_coal),"Уголь с данными характеристиками можно использовать в доменных печах")
